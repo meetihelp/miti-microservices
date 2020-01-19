150601ce8279b354786b862d740a427f881da876
Gaurav Jha <gjhakgp@github.com>
2020-01-19 10:34:47 +0530

    get primary trust chain

--- Notification/SMS/send_alertMessage.go
+++ Notification/SMS/send_alertMessage.go
@@ -7,5 +7,7 @@ import(
 func AlertNotification(phoneList []string,name string,latitude string,longitude string) string{
 	fmt.Print("Sending Alert Message to:")
 	fmt.Println(phoneList)
+	msg:=name+" is in danger at latitude:"+latitude+",longitude:"+longitude
+	fmt.Println(msg)
 	return "Ok"
 }
